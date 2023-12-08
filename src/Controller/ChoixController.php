5d60f709ca330e6389ee461ad37c127b24a7a87f
Matis GASSE <matis-28-03@hotmail.com>
2023-12-08 05:26:57 +0100

    change point stats

--- src/Controller/ChoixController.php
+++ src/Controller/ChoixController.php
@@ -8,18 +8,31 @@ use App\Repository\PartieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\StatistiqueRepository;
+use App\Repository\InflueRepository;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class ChoixController extends AbstractController
 {
+
+    public function __construct(
+        private Security $security,
+    ){
+    }
+
     #[Route('/explication/{nbr}', name: 'app_explication', methods: ['GET'])]
-    public function index(int $nbr , ChoixRepository $choixRepository, ExplicationRepository $explicationRepository, PartieRepository $partieRepository): Response
+    public function index(int $nbr , ChoixRepository $choixRepository, ExplicationRepository $explicationRepository, PartieRepository $partieRepository, StatistiqueRepository $statistiqueRepository, InflueRepository $influeRepository): Response
     {
         $choix = $choixRepository->findById($nbr);
         $explication = $explicationRepository->findByChoix($choix);
         $partie = $partieRepository->getByUser($this->security->getUser());
-        $partie->setScore($partie->getScore + 1);
+        $partie->setScore($partie->getScore() + 1);
 
-        
+        $stats = $statistiqueRepository->findByPartie($partie);
+
+        for($i=0;$i<3;$i++){
+            $stats[$i]->setPoints($stats[$i]->getPoints() + $influeRepository->getByChoix($choixRepository->getOnById($_GET[$id])));
+        }
 
 
         return $this->render('explication/index.html.twig', [
