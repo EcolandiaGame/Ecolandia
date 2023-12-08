fa179a7ca76bce901f483884c13600115636fbe7
Matis GASSE <matis-28-03@hotmail.com>
2023-12-08 02:53:54 +0100

    background image

--- src/Controller/ChoixController.php
+++ src/Controller/ChoixController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Repository\ChoixRepository;
 use App\Repository\ExplicationRepository;
-
+use App\Repository\PartieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,10 +12,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class ChoixController extends AbstractController
 {
     #[Route('/explication/{nbr}', name: 'app_explication')]
-    public function index(int $nbr , ChoixRepository $choixRepository, ExplicationRepository $explicationRepository): Response
+    public function index(int $nbr , ChoixRepository $choixRepository, ExplicationRepository $explicationRepository, PartieRepository $partieRepository): Response
     {
-         $choix = $choixRepository->findById($nbr);
-         $explication = $explicationRepository->findByChoix($choix);
+        $choix = $choixRepository->findById($nbr);
+        $explication = $explicationRepository->findByChoix($choix);
+        $partie = $partieRepository->getByUser($this->security->getUser());
+        $partie->setScore($partie->getScore + 1);
 
 
         return $this->render('choix/index.html.twig', [
