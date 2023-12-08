7f8eacf20a53aa35db8b4b776d160802d2ff09c8
Benjamin Perez <95043947+benji28000@users.noreply.github.com>
2023-12-08 04:58:51 +0100

    verif/gameover

--- src/Controller/EcolandiaController.php
+++ src/Controller/EcolandiaController.php
@@ -7,9 +7,12 @@ use App\Repository\StatistiqueRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class EcolandiaController extends AbstractController
 {
+
+
     #[Route('/ecolandia', name: 'app_ecolandia')]
     public function index(): Response
     {
@@ -24,16 +27,40 @@ class EcolandiaController extends AbstractController
         );
     }
 
+    public function __construct(
+        private RequestStack $requestStack
+    ){
+    }
+
     #[Route('/ecolandia/verif', name: 'app_verif')]
-    public function verif(PartieRepository $partieRepository, StatistiqueRepository $statistiqueRepository): Response
+    public function verif(PartieRepository $partieRepository, StatistiqueRepository $statistiqueRepository, RequestStack $requestStack): Response
     {
+
+        $session = $this->requestStack->getSession();
+        $indice = $session->get('indiceevent');
+
+
+
+
+        $session->set('indiceevent', $indice+1);
+        $indice_after = $session->get('indiceevent');
+
+
+
         $PartieActuelleId = $partieRepository->getByLastId();
 
         $statistique= $statistiqueRepository->findBy(['partie' => $PartieActuelleId]);
-        dd($statistique);
+
+        foreach ($statistique as $stat) {
+            if($stat->getPoints()==0){
+                return $this->render('defeat/gameover.html.twig',
+                );
+            }
+        }
 
 
-        return $this->render('ecolandia/verif.html.twig',
+
+        return $this->render('ecolandia/game.html.twig',
         );
     }
 }
